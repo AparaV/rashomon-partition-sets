5b0712e0985f0941173ec28e18dd786b6a49afb9
Apara Venkat <apara.vnkat@gmail.com>
2023-11-15 10:26:22 -0700

    Fix Flake8 formatting errors

--- Code/rashomon/tva.py
+++ Code/rashomon/tva.py
@@ -2,28 +2,30 @@ import itertools
 import numpy as np
 
 
-##
-## Enumerate policies
-##
+#
+# Enumerate policies
+#
+
 
 def __enumerate_policies_classic__(M, R):
     # All arms have the same intensities
-    intensities = np.arange(R-1) + 1
+    intensities = np.arange(R - 1) + 1
     policies = []
     for pol in itertools.product(intensities, repeat=M):
         policies.append(pol)
     return policies
 
+
 def __enumerate_policies_complex__(R):
     # Each arm may have different intensities
     intensities = []
     for Ri in R:
-        intensities.append(np.arange(Ri-1) + 1)
+        intensities.append(np.arange(Ri - 1) + 1)
     policies = []
     for pol in itertools.product(*intensities, repeat=1):
         policies.append(pol)
     return policies
-        
+
 
 def enumerate_policies(M, R):
     if isinstance(R, int) or len(R) == 1:
