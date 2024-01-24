8e203ab4f279c98adfc39e8a84b7153bf60f013e
Apara Venkat <apara.vnkat@gmail.com>
2024-01-23 23:07:51 -0800

    Run the NHANES data end to end

--- Code/rashomon/aggregate/__init__.py
+++ Code/rashomon/aggregate/__init__.py
@@ -1,7 +1,13 @@
 from .profile import RAggregate_profile
 from .raggregate import RAggregate
+from .raggregate import find_profile_lower_bound
+from .raggregate import find_feasible_combinations
+from .raggregate import remove_unused_poolings
 
 __all__ = [
     "RAggregate_profile",
-    "RAggregate"
+    "RAggregate",
+    "find_profile_lower_bound",
+    "find_feasible_combinations",
+    "remove_unused_poolings"
 ]
