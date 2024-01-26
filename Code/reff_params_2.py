508e086bf8e217364a32686868711472c8c4c045
Apara Venkat <apara.vnkat@gmail.com>
2024-01-25 16:49:53 -0800

    Sims - good ones?

--- Code/reff_params_2.py
+++ Code/reff_params_2.py
@@ -26,30 +26,30 @@ var_2 = np.array([1])
 sigma_3 = np.array([[1, 1],
                     [1, 0]])
 mu_3 = np.array([0, 1])
-var_3 = np.array([1, 2])
+var_3 = np.array([1, 1])
 
 # Profile 4: (1, 0, 0)
 sigma_4 = np.array([[1, 1]])
-mu_4 = np.array([1])
+mu_4 = np.array([3.8])
 var_4 = np.array([1])
 
 # Profile 5: (1, 0, 1)
 sigma_5 = np.array([[1, 1],
                     [1, 0]])
 mu_5 = np.array([0, 4])
-var_5 = np.array([1, 1])
+var_5 = np.array([1, 3])
 
 # Profile 6: (1, 1, 0)
 sigma_6 = np.array([[1, 1],
                     [1, 0]])
 mu_6 = np.array([0, 1])
-var_6 = np.array([1, 2])
+var_6 = np.array([1, 1])
 
 # Profile 1: (1, 1, 1)
 sigma_7 = np.array([[1, 1],
                     [1, 1],
                     [1, 0]])
-mu_7 = np.array([0, 3.8])
+mu_7 = np.array([0, 1])
 var_7 = np.array([1, 1])
 
 sigma = [sigma_0, sigma_1, sigma_2, sigma_3, sigma_4, sigma_5, sigma_6, sigma_7]
