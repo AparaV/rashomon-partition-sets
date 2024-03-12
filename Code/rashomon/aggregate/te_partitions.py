7b50922a9e8da1c573c98e458855e4e532fcf7fe
Apara Venkat <apara.vnkat@gmail.com>
2024-03-12 14:01:57 -0700

    Add argument to normalize loss correctly

--- Code/rashomon/aggregate/te_partitions.py
+++ Code/rashomon/aggregate/te_partitions.py
@@ -25,18 +25,20 @@ def get_intersection_matrix(ctl_pools, trt_pools, all_policies, trt_arm_idx):
     return sigma_int
 
 
-def het_partition_solver(D_tc, y_tc, policy_means, trt_pools, ctl_pools, col, sigma_int, theta, P_qe, reg):
+def het_partition_solver(D_tc, y_tc, policy_means, trt_pools, ctl_pools, col, sigma_int, theta, P_qe, reg,
+                         normalize=0):
     ncols = sigma_int.shape[1]
     if col == ncols:
         return P_qe
 
-    Q_0 = compute_het_Q(D_tc, y_tc, sigma_int, trt_pools, ctl_pools, policy_means, reg)
+    Q_0 = compute_het_Q(D_tc, y_tc, sigma_int, trt_pools, ctl_pools, policy_means, reg, normalize=normalize)
     if Q_0 <= theta:
         if not P_qe.seen(sigma_int):
             P_qe.insert(sigma_int.copy())
             P_qe.Q = np.append(P_qe.Q, Q_0)
         P_qe = het_partition_solver(
-            D_tc, y_tc, policy_means, trt_pools, ctl_pools, col+1, sigma_int, theta, P_qe, reg)
+            D_tc, y_tc, policy_means, trt_pools, ctl_pools, col+1, sigma_int, theta, P_qe, reg,
+            normalize=normalize)
 
     zero_loc = np.where(sigma_int[:, col] == 0)[0]
     nz = len(zero_loc)
@@ -48,13 +50,14 @@ def het_partition_solver(D_tc, y_tc, policy_means, trt_pools, ctl_pools, col, si
         sigma_tmp[:, col] = np.inf
         sigma_tmp[row, col] = 1
 
-        Q_i = compute_het_Q(D_tc, y_tc, sigma_tmp, trt_pools, ctl_pools, policy_means, reg=1e-1)
+        Q_i = compute_het_Q(D_tc, y_tc, sigma_tmp, trt_pools, ctl_pools, policy_means, reg=1e-1, normalize=normalize)
         if Q_i <= theta:
             if not P_qe.seen(sigma_tmp):
                 P_qe.insert(sigma_tmp.copy())
                 P_qe.Q = np.append(P_qe.Q, Q_i)
             P_qe = het_partition_solver(
-                D_tc, y_tc, policy_means, trt_pools, ctl_pools, col+1, sigma_int, theta, P_qe, reg)
+                D_tc, y_tc, policy_means, trt_pools, ctl_pools, col+1, sigma_int, theta, P_qe, reg,
+                normalize=normalize)
 
     return P_qe
 
@@ -66,7 +69,7 @@ def find_te_het_partitions(
         trt_arm_idx,
         all_policies, policies_ids_profiles,
         D_tc, y_tc, policy_means,
-        theta, reg) -> RashomonSet:
+        theta, reg, normalize=0) -> RashomonSet:
 
     trt_pools_0, _ = extract_pools(trt_policies, sigma_t)
     ctl_pools_0, _ = extract_pools(ctl_policies, sigma_c)
@@ -80,6 +83,7 @@ def find_te_het_partitions(
     sigma_int = get_intersection_matrix(ctl_pools, trt_pools, all_policies, trt_arm_idx)
 
     P_qe = RashomonSet(sigma_int.shape)
-    P_qe = het_partition_solver(D_tc, y_tc, policy_means, trt_pools, ctl_pools, 0, sigma_int, theta, P_qe, reg)
+    P_qe = het_partition_solver(D_tc, y_tc, policy_means, trt_pools, ctl_pools, 0, sigma_int,
+                                theta, P_qe, reg, normalize)
 
     return P_qe
