8b346e6340eb0da1e90a570ee762ce0c29b6d71e
Apara Venkat <apara.vnkat@gmail.com>
2024-07-15 15:01:51 -0700

    Add docstrings for tva.py

--- Code/rashomon/tva.py
+++ Code/rashomon/tva.py
@@ -2,13 +2,8 @@ import itertools
 import numpy as np
 
 
-#
-# Enumerate policies
-#
-
-
-def __enumerate_policies_classic__(M, R):
-    # All arms have the same intensities
+def __enumerate_policies_classic__(M: int, R: int) -> list:
+    """ Enumerate all features for M arms each with fixed R factor levels """
     intensities = np.arange(R)
     policies = []
     for pol in itertools.product(intensities, repeat=M):
@@ -16,8 +11,8 @@ def __enumerate_policies_classic__(M, R):
     return policies
 
 
-def __enumerate_policies_complex__(R):
-    # Each arm may have different intensities
+def __enumerate_policies_complex__(R: np.array) -> list:
+    """ Enumerate all features with varying R factor levels """
     intensities = []
     for Ri in R:
         intensities.append(np.arange(Ri))
@@ -27,7 +22,18 @@ def __enumerate_policies_complex__(R):
     return policies
 
 
-def enumerate_policies(M, R):
+def enumerate_policies(M: int, R: int | np.array) -> list:
+    """
+    Enumerate all possible features
+
+    Arguments:
+    M (int): Number of arms
+    R (int | np.array): Number of factor levels for each arm.
+        If type is of int, all arms have same number of factor levels
+
+    Returns:
+    policies (list): List of all possible features
+    """
     if isinstance(R, int) or len(R) == 1:
         if not isinstance(R, int):
             R = R[0]
@@ -37,7 +43,18 @@ def enumerate_policies(M, R):
     return policies
 
 
-def enumerate_profiles(M):
+def enumerate_profiles(M: int) -> tuple[list, dict[tuple, int]]:
+    """
+    Enumerate profiles for M arms
+
+    Arguments:
+    M (int): Number of arms
+
+    Returns:
+    (profiles, profile_map)
+        profiles (list): List of all possible profiles
+        profile_map (dict[tuple, int]): Mapping from profile to index in profiles
+    """
     profiles = itertools.product([0, 1], repeat=M)
     profiles = [x for x in profiles]
     profile_map = {}
@@ -46,12 +63,14 @@ def enumerate_profiles(M):
     return profiles, profile_map
 
 
-def policy_to_profile(policy):
+def policy_to_profile(policy: tuple) -> tuple:
+    """ Identifies the profile that the feature belongs to """
     profile = tuple([int(x > 0) for x in policy])
     return profile
 
 
-def weakly_dominates(x, y):
+def weakly_dominates(x: tuple, y: tuple) -> bool:
+    """ Check if feature x weakly dominates feature y """
     M = len(x)
     dominates = True
     for i in range(M):
@@ -64,7 +83,16 @@ def weakly_dominates(x, y):
     return dominates
 
 
-def alpha_matrix(policies):
+def alpha_matrix(policies: list) -> np.ndarray:
+    """
+    Construct the TVA alpha matrix transformation
+
+    Arguments:
+    policies (list): List of features
+
+    Returns:
+    G (np.ndarray): Alpha matrix
+    """
     n = len(policies)
     G = np.ndarray(shape=(n, n))
     G[:, :] = 0
@@ -80,7 +108,18 @@ def alpha_matrix(policies):
     return G
 
 
-def get_dummy_matrix(D, G, num_policies):
+def get_dummy_matrix(D: np.ndarray, G: np.ndarray, num_policies: int) -> np.ndarray:
+    """
+    Construct the dummy matrix for TVA
+
+    Arguments:
+    D (np.ndarray): Data matrix
+    G (np.ndarray): Alpha matrix
+    num_policies (int): Number of policies
+
+    Returns:
+    D_matrix (np.ndarray): Dummy matrix
+    """
     num_data, _ = D.shape
     D_matrix = np.ndarray(shape=(num_data, num_policies))
     D_matrix[:, :] = 0
@@ -90,15 +129,18 @@ def get_dummy_matrix(D, G, num_policies):
     return D_matrix
 
 
-def profile_ids_to_univ_ids(pi_pools_0, univ_pol_id_list):
+def profile_ids_to_univ_ids(pi_pools_0: dict[int, list[int]],
+                            univ_pol_id_list: list[int]) -> dict[int, list[int]]:
     """
-    pi_pools_0: Dictionary. key is pool ID, value is list of policy IDs within that profile
-    univ_pol_id_list: List of global policy IDs for profile corresponding to pi_pools_0
+    Convert policy IDs in pi_pools_0 to global policy IDs
+
+    Arguments:
+    pi_pools_0 (dict[int, list[int]]): key is pool ID, value is list of policy IDs within that profile
+    univ_pol_id_list (list[int]): List of global policy IDs for profile corresponding to pi_pools_0
         Policy with profile ID `i` has global ID `univ_pol_id_list[i]`
 
     Returns:
-        pi_pools: Dictionary identical to pi_pools_0.
-            Policy IDs for each pool now correspond to global policy ID
+    pi_pools (dict[int, list[int]]): Policy IDs for each pool now correspond to global policy ID
     """
     pi_pools = {}
     for pi_id, pol_idx_list in pi_pools_0.items():
@@ -107,15 +149,17 @@ def profile_ids_to_univ_ids(pi_pools_0, univ_pol_id_list):
     return pi_pools
 
 
-def remove_arm(policies_list, arm_idx):
+def remove_arm(policies_list: list[tuple], arm_idx: int) -> list[tuple]:
     """
     Removes dosage corresponding to arm_idx in policies_list
 
-    policies_list: List of tuples corresponding to policy dosages
+    Arguments:
+    policies_list (list): List of tuples corresponding to policy dosages
+    arm_idx (int): Index of arm to remove
 
     Returns:
-        policies_list: List of tuples corresponding to policy dosages where arm_idx
-            is removed
+    policies_list (list[tuple]): List of tuples corresponding to feature levels
+        after arm_idx is removed
     """
     policies_list = [tuple(pol[:arm_idx] + pol[(arm_idx+1):]) for pol in policies_list]
     return policies_list
